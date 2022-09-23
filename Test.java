b019a4bd221c8ba6fc8e9a55d42775dd82480c49
Su1030 <114204259+Su1030@users.noreply.github.com>
2022-09-23 11:59:57 +0800

    3

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
 public class Test{
     public static void main(String[] args){
+        System.out.println(args[0]);
+        countNumber("ouo");
+    }
+    public static void countNumber(String str){
         for(var i = 0; i <= 10; i++){
-            System.out.println(i);
+            System.out.println(str + i);
         }
     }
 }
